6781b3e2bb7460c478c91524142461a053576793
RESUL COŞKUN <49487234+rslcskn@users.noreply.github.com>
2021-10-09 19:49:42 +0300

    Create locked-user.ps1

---
+++ locked-user.ps1
@@ -0,0 +1,20 @@
+$tarih= (get-date).ToString("dd-MM-yy")
+
+Search-ADAccount -LockedOut -UsersOnly -ResultPageSize 2000 -resultSetSize $null | Select-Object Name, SamAccountName, DistinguishedName | Export-CSV “C:\Temp\LockedOutUsers-$tarih.csv” -NoTypeInformation
+
+$MailSender = " Gönderici Adı <Mail Adresi>" #Örn; Resul <resul@resul.info#
+$Subject = "Mail Başlığı" #Mail başlığı giriniz.#
+$EmailBody = "Kilitli kullanıcılar ektedir."
+$SMTPServer = 'SMTP Adresi' #SMTP adresini giriniz.#
+$filepath = "C:\Temp\LockedOutUsers-$tarih.csv" 
+
+If ((Get-Item $filepath).Length -gt 0kb) { 
+Send-MailMessage -Encoding UTF8 -To "Alıcı Mail adresi" `
+-From $MailSender -SmtpServer $SMTPServer `
+-Subject $Subject `
+-BodyAsHtml $EmailBody `
+-Attachments $filepath `
+
+}
+else
+{}
